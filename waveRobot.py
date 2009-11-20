ab64c7d83676b70ddbe6120018914820a9fbf2db
morimasa <moriya.masaharu+git@gmail.com>
2009-11-20 19:02:41 +0900

    mod names

--- waveRobot.py
+++ waveRobot.py
@@ -1,3 +1,4 @@
+import logging
 from waveapi import events
 from waveapi import model
 from waveapi import robot
@@ -7,24 +8,25 @@ def OnParticipantsChanged(properties, context):
   for participant in added:
     Greet(context, participant)
  
-def OnRobotAdded(properties, context):
-  root_wavelet = context.GetRootWavelet()
-  root_wavelet.CreateBlip().GetDocument().SetText('Hi, everybody!')
- 
-def OnBlipCreated(properties, context):
-  blip = context.GetBlipById(properties['blipId'])
-  blip.GetDocument().AppendText(':-)')
- 
 def Greet(context, participant):
   root_wavelet = context.GetRootWavelet()
   root_wavelet.CreateBlip().GetDocument().SetText('Welcome, ' + participant)
  
+def OnRobotAdded(properties, context):
+  root_wavelet = context.GetRootWavelet()
+  root_wavelet.CreateBlip().GetDocument().SetText('Hi, everybody!')
+ 
+def OnBlipSubmitted(properties, context):
+  logging.info('called OnBlipSubmitted')
+  blip = context.GetBlipById(properties['blipId'])
+  blip.GetDocument().AppendText(';-)')
+
 if __name__ == '__main__':
   myRobot = robot.Robot('waveRobot',
       image_url='http://wave-robot-morimasa.appspot.com/assets/icon.png',
-      version='1',
+      version='4',
       profile_url='http://wave-robot-morimasa.appspot.com/')
   myRobot.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged)
-  myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipCreated)
+  myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitted)
   myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
   myRobot.Run()
